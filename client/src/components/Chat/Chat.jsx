be69517cc4e47ea42639816c6eb67e7e0bea04ca
Bruno Castro <bcastroduarte12@gmail.com>
2023-02-22 21:21:18 -0300

    Styling

--- client/src/components/Chat/Chat.jsx
+++ client/src/components/Chat/Chat.jsx
@@ -1,4 +1,7 @@
 import React, {useRef, useState, useEffect} from 'react'
+import {Input} from '@mui/material'
+import SendIcon from '@mui/icons-material/Send';
+import style from './Chat.module.css'
 
 export default function Chat({socket}) {
 
@@ -27,14 +30,22 @@ export default function Chat({socket}) {
 
   return (
     <div>
-      <h1>Chat</h1>
-      {
-        messageList.map((message,index) => (
-          <p key={index}>{message.author}: {message.text}</p>
-        ))
-      }
-      <input type="text" ref={messageRef} placeholder='Mensagem' />
-      <button onClick={()=>handleSubmit()}>Enviar</button>
+      <div className={style['chat-container']}>
+        <div className={style["chat-body"]}>
+        {
+          messageList.map((message,index) => (
+            <div className={`${style["message-container"]} ${message.authorId === socket.id && style["message-mine"]}`} key={index}>
+              <div className="message-author"><strong>{message.author}</strong></div>
+              <div className="message-text">{message.text}</div>
+            </div>
+          ))
+        }
+        </div>
+        <div className={style["chat-footer"]}>
+          <Input inputRef={messageRef} placeholder='Mensagem' fullWidth />
+          <SendIcon sx={{m:1, cursor: 'pointer'}} onClick={()=>handleSubmit()} color="primary" />
+        </div>
+      </div>
     </div>
   )
 }
--- client/src/components/Join/Join.jsx
+++ client/src/components/Join/Join.jsx
@@ -1,5 +1,7 @@
 import React, {useRef} from 'react'
 import io from 'socket.io-client'
+import style from './Join.module.css'
+import {Input, Button} from '@mui/material'
 
 export default function Join({setChatVisibility, setSocket}) {
 
@@ -15,10 +17,10 @@ export default function Join({setChatVisibility, setSocket}) {
   }
 
   return (
-    <div>
-      <h1>Join</h1>
-      <input type="text" ref={usernameRef} placeholder='Nome de usuário' />
-      <button onClick={()=>handleSubmit()}>Entrar</button>
+    <div className={style['join-container']}>
+      <h2>Chat em tempo real</h2>
+      <Input inputRef={usernameRef} placeholder='Nome de usuário' />
+      <Button sx={{mt:2}} onClick={()=>handleSubmit()} variant="contained">Entrar</Button>
     </div>
   )
 }
